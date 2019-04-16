1914cd8a64ef3c9090a00c65b1e2b5c04ea3ae03
Bruce Liu <ccieliu@gmail.com>
2019-04-16 15:46:59 +0800

    Change interval to 30s

--- csoneplugin.user.js
+++ csoneplugin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSone Queue Refresh
 // @namespace    https://csone.my.salesforce.com/console
-// @version      0.0.2
+// @version      0.0.3
 // @description  Auto Refresh
 // @author       yuxuliu@cisco.com
 // @match        https://csone.my.salesforce.com/500*
@@ -14,11 +14,11 @@
 // Version History :
 // 0.0.1    First release
 // 0.0.2    Fix the queue button, useClassname get elements
-
+// 0.0.3    Fix the queue button, useClassname get elements
 (function() {
     'use strict';
 function refreshQueue(){
     document.getElementsByClassName("btn refreshListButton")[0].onclick()
 }
-setInterval(refreshQueue,5000);
+setInterval(refreshQueue,30000);
 })();
