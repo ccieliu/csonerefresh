1c8d797ea3e189d6aea616a4ab3cb2ca66c5aba0
Bruce Lau <ccieliu@gmail.com>
2020-04-24 18:43:25 +0800

    Update csoneplugin.user.js

--- csoneplugin.user.js
+++ csoneplugin.user.js
@@ -11,18 +11,15 @@
 // @downloadURL https://raw.githubusercontent.com/ccieliu/csonerefresh/master/csoneplugin.user.js
 // @updateURL   https://raw.githubusercontent.com/ccieliu/csonerefresh/master/csoneplugin.user.js
 // ==/UserScript==
-
 // Version History :
 // 0.0.1    First release
 // 0.0.2    Fix the queue button, useClassname get elements
 // 0.0.3    Change interval to 30s
 // 0.0.4    Add case notification feature
-
 function refreshQueue(){
     try{ document.getElementsByClassName("btn refreshListButton")[0].onclick() }
     catch(err){}
 }
-
 function notiCase(){
     try{
                 var caseSubject = document.getElementsByClassName("x-grid3-cell-inner x-grid3-col-CASES_SUBJECT")[0].textContent
@@ -36,8 +33,6 @@ function notiCase(){
     GM_notification(notificationDetails);
     }
     catch(err){}
-
-
 }
 setInterval(refreshQueue,30000);
 setInterval(notiCase,20000);
