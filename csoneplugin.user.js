0f0c62f03d95d3260f7576aa0b23355064b5ec59
Bruce Lau <ccieliu@gmail.com>
2020-04-24 18:39:59 +0800

    Add case notification feature
    GM_notification

--- csoneplugin.user.js
+++ csoneplugin.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         CSone Queue Refresh
 // @namespace    https://csone.my.salesforce.com/console
-// @version      0.0.3
+// @version      0.0.4
 // @description  Auto Refresh
 // @author       yuxuliu@cisco.com
 // @match        https://csone.my.salesforce.com/500*
-// @grant        none
+// @grant       GM_notification
+// @grant       window.focus
 // @run-at      document-idle
 // @downloadURL https://raw.githubusercontent.com/ccieliu/csonerefresh/master/csoneplugin.user.js
 // @updateURL   https://raw.githubusercontent.com/ccieliu/csonerefresh/master/csoneplugin.user.js
@@ -15,10 +16,28 @@
 // 0.0.1    First release
 // 0.0.2    Fix the queue button, useClassname get elements
 // 0.0.3    Change interval to 30s
-(function() {
-    'use strict';
+// 0.0.4    Add case notification feature
+
 function refreshQueue(){
-    document.getElementsByClassName("btn refreshListButton")[0].onclick()
+    try{ document.getElementsByClassName("btn refreshListButton")[0].onclick() }
+    catch(err){}
+}
+
+function notiCase(){
+    try{
+                var caseSubject = document.getElementsByClassName("x-grid3-cell-inner x-grid3-col-CASES_SUBJECT")[0].textContent
+        var notificationDetails = {
+            text: caseSubject,
+            title: 'NEW CASE!',
+            timeout: 10000,
+            image: 'https://i.stack.imgur.com/geLPT.png',
+            onclick: function() { window.focus(); },
+        };
+    GM_notification(notificationDetails);
+    }
+    catch(err){}
+
+
 }
 setInterval(refreshQueue,30000);
-})();
+setInterval(notiCase,20000);
